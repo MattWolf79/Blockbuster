8196a0a2e0ff7ea23d61dbe97e48b2be157c6954
CTIMOVIL\EXA19447 <matias.lobo@claro.com.ar>
2023-07-10 22:23:51 -0300

    Entrega10072023

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import NavBar from './components/Navbar/Navbar';
+import NavBar from './components/NavBar/NavBar';
 import Banner from './components/Banner/Banner';
 
 function App() {  
